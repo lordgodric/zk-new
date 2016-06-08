ca538d1bfadb5a5c89a1bbbc35081483d75bd58d
lordgodric <elric@ex.ua>
2016-06-08 14:43:48 +0300

    add category works fine now )

--- js/categories.js
+++ js/categories.js
@@ -1,67 +1,80 @@
 $(document).ready(function() {
-   // Stuff to do as soon as the DOM is ready
+    // Stuff to do as soon as the DOM is ready
 
-   $('.e-category-item').click(function(event) {
-     // try find #hashtag in textarea
-     var present = str.search($(this).find($('.category-hash')).text());
-     // take #hashtag name value from div
-     var thisHash = $(this).find($('.category-hash')).text();
 
-     // if textarea null
-     if (!$('.request-text').val()) {
-      $('.request-text').val($('.request-text').val() + thisHash);
-      thisHash = thisHash + ' ';
 
-      // if textarea not null
-     } else {
-       thisHash = ' ' + thisHash + ' ';
-       console.log(present);
+    $('.e-category-item').click(function(event) {
 
-       // check availability #hashtag in textarea
-       if (present == -1) {
+        // take #hashtag name value from div
+        var thisHash = $(this).find($('.category-hash')).text();
+        var str = $('.request-text').val();
+        var present = str.search(thisHash);
 
-         // add #hashtag
-         $('.request-text').val($('.request-text').val() + thisHash);
+        // if textarea null
+        if (!$('.request-text').val()) {
+            thisHash = thisHash + ' ';
+            $('.request-text').val($('.request-text').val() + thisHash);
 
-       } else {
+            $(this).addClass('border-green');
+            $(this).removeClass('hvr-underline-from-center');
 
-         // delete #hashtag
-         $('.request-text').val($('.request-text').val().replace(thisHash, ''));
-       }
-     }
-   });
+        // if textarea not null
+        } else {
 
-   $('.e-category-item').mouseover(function(event) {
-     var thisDesc = $(this).find($('.category-desc')).text();
-     $(this).find($('.category-desc'));
-     $('.output-category').text(thisDesc);
-     $('.e-category-item').mouseleave(function(event) {
-     });
-   });
+            thisHash = ' ' + thisHash + ' ';
+            // if we don't have thisHash in textarea
+            if (present == -1) {
 
-   $('.address-value').change(function(event) {
-    // if address field not empty
-    if ($('.address-value').val()) {
-      $('.map-details-btn').addClass('border-green');
-      $('.map-details-btn').removeClass('hvr-underline-from-center');
-    // if address field empty
-    } else {
-      $('.map-details-btn').addClass('hvr-underline-from-center');
-      $('.map-details-btn').removeClass('border-green');
-    }
-   });
+                // add #hashtag
+                $('.request-text').val($('.request-text').val() + thisHash);
 
-   $('.upload-wrapper').change(function(event) {
-     /* Act on the event */
-     if ($('.image-preview-filename')) {
-       $('.image-details-btn').addClass('border-green');
-       $('.image-details-btn').removeClass('hvr-underline-from-center');
-     };
-     $('.image-preview-clear').click(function(event) {
-       $('.image-details-btn').addClass('hvr-underline-from-center');
-       $('.image-details-btn').removeClass('border-green');
-     });
-   });
+                $(this).addClass('border-green');
+                $(this).removeClass('hvr-underline-from-center');
+
+            // if we have #hashtag in textarea
+            } else {
+                console.log(present);
+
+                // delete #hashtag in text - not at first word
+                $('.request-text').val($('.request-text').val().replace(thisHash, ''));
+
+                $(this).removeClass('border-green');
+                $(this).addClass('hvr-underline-from-center');
+            }
+
+        }
+    });
+
+    $('.e-category-item').mouseover(function(event) {
+        var thisDesc = $(this).find($('.category-desc')).text();
+        $(this).find($('.category-desc'));
+        $('.output-category').text(thisDesc);
+        $('.e-category-item').mouseleave(function(event) {});
+    });
+
+    $('.address-value').change(function(event) {
+        // if address field not empty
+        if ($('.address-value').val()) {
+            $('.map-details-btn').addClass('border-green');
+            $('.map-details-btn').removeClass('hvr-underline-from-center');
+            // if address field empty
+        } else {
+            $('.map-details-btn').addClass('hvr-underline-from-center');
+            $('.map-details-btn').removeClass('border-green');
+        }
+    });
+
+    $('.upload-wrapper').change(function(event) {
+        /* Act on the event */
+        if ($('.image-preview-filename')) {
+            $('.image-details-btn').addClass('border-green');
+            $('.image-details-btn').removeClass('hvr-underline-from-center');
+        };
+        $('.image-preview-clear').click(function(event) {
+            $('.image-details-btn').addClass('hvr-underline-from-center');
+            $('.image-details-btn').removeClass('border-green');
+        });
+    });
 
 
 
